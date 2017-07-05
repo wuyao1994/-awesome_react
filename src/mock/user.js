0feedec0e5305658a8857927b8db6904b0c38af8
wuyao1994 <wuyaoiot@163.com>
2017-07-05 17:34:51 +0800

    test git config

---
+++ src/mock/user.js
@@ -0,0 +1,18 @@
+const adminUsers = [
+  {
+    id: 0,
+    username: 'admin',
+    password: 'admin',
+    permissions: userPermission.ADMIN,
+  }, {
+    id: 1,
+    username: 'guest',
+    password: 'guest',
+    permissions: userPermission.DEFAULT,
+  }, {
+    id: 2,
+    username: '吴彦祖',
+    password: '123456',
+    permissions: userPermission.DEVELOPER,
+  },
+]
